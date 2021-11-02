1f311a1a4821d95170cb207ae82288e8f704a088
unknown <aminkachout@gmail.com>
2021-11-02 20:58:44 +0100

    calcule taxe

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 const express = require('express')
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+var router = express.Router();
+
 const port = 3000
 
 app.get('/test', (req,res)=> {
     res.send('hello')
+})
+app.post('/calcule', (req,res)=> {
+    res.send( req.body)
 })
 app.listen(port,()=>{
     console.log('Express runing ');
